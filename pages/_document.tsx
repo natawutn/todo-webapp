b361084d95e934ec59b1bfa4895aae2d84c739b0
Saenyakorn Siangsanoh <saenyakorn.si@gmail.com>
2022-11-26 01:20:52 +0700

    fix script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html>
       <Head>
-        <script type='text/javascript' src='/__ENV.js' async />
+        <Script src='/__ENV.js' strategy='beforeInteractive' type='text/javascript' />
       </Head>
       <title>Uber to do</title>
       <body>
